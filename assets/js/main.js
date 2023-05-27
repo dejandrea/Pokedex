33ce5044aba3892a3a1fce0b7d9546fed2b1b3d6
dejandrea <dejandrea@gmail.com>
2023-05-27 20:45:47 -0300

    integrando com a pokeApi

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
-console.log("sucesso!");
+convertPokemonToLi = (pokemon, index) => {
+  index++;
+  let cod;
+  if (index < 10) {
+    cod = "#00"+index;
+  } else {
+    cod = "#0"+index;
+  }
+  return `   
+        <li class="pokemon">
+            <span class="number">${cod}</span>
+            <span class="name">${pokemon.name}</span>
+
+            <div class="detail">
+                <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+                </ol>
+                <img
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${index}.svg"
+                alt="${pokemon.name}"
+                />
+            </div>
+        </li>
+        `;
+};
+
+pokeApi.getPokemons(0,20).then((pokemonList = []) => {
+  // debugger;
+
+  document.getElementById("pokemonList").innerHTML += pokemonList
+    .map(convertPokemonToLi)
+    .join("");
+});
